a91c22701af9638dfa9bcf356cf21697a3670afc
Ravi Sen <ravisen2011@gmail.com>
2015-07-06 20:11:47 +0530

    First commit.

---
+++ ToDoAppNew/app.ts
@@ -0,0 +1,78 @@
+﻿'use strict';
+var app = angular.module("app", ["firebase"]); 
+
+app.factory("todoFactory", ["$firebaseArray", ($firebaseArray) => {
+    var ref = new Firebase("https://amber-torch-288.firebaseio.com/ToDOApp");
+    return $firebaseArray(ref);
+}]);
+
+
+app.controller("ToDoAppController", ["$scope", "todoFactory", ($scope, todoFactory)=> {
+    $scope.newTodo = "";
+    $scope.masterCheckBoxStatus = false;
+    $scope.todos = todoFactory;
+    $scope.addTodo = function () {
+        $scope.todos.$add({
+            noteText: $scope.newTodo,
+            createdDate: $scope.getFormattedDate(),
+            isDone:false,
+            inEditMode:false
+        });
+        $scope.newTodo = "";
+    }
+    $scope.addTodo = function () {
+        $scope.todos.$add({
+            noteText: $scope.newTodo,
+            createdDate: $scope.getFormattedDate(),
+            isDone: false,
+            inEditMode: false
+        });
+        $scope.newTodo = "";
+    }
+
+    $scope.todos.$loaded(() => {
+        if ($scope.todos.length === 0) {
+            console.log("Empty List.");
+        } else {
+            var status = false;
+            var trueCount = 0;
+            var falseCount = 0;
+            angular.forEach($scope.todos,(k) => {
+                status = k.isDone;
+                if (k.isDone) {
+                    trueCount++;
+                } else {
+                    falseCount++;
+                }
+            });
+            if (trueCount === $scope.todos.length || falseCount === $scope.todos.length)
+                $scope.masterCheckBoxStatus = status;
+            else
+                $scope.masterCheckBoxStatus = false;
+        }
+    });
+    $scope.edit = function (todo) {
+        todo.inEditMode = true;
+    }
+
+    $scope.save = function (todo) {
+        todo.inEditMode = false;
+        $scope.todos.$save(todo);
+    }
+
+    $scope.selectAll = function () {
+        angular.forEach($scope.todos,(i) => { i.isDone = $scope.masterCheckBoxStatus; $scope.todos.$save(i); });
+        return true;
+    }
+
+    $scope.getFormattedDate = function () {
+        var _date = new Date();
+        var month = _date.getMonth() + 1;
+        var day = _date.getDate();
+        var year = _date.getFullYear();
+        var formattedDate = month + "/" + day + "/" + year;
+        console.log(formattedDate);
+        return formattedDate;
+    }
+
+}])
